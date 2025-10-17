ab0a2d43dcb66b1616ec79d63b95bb2edba26aed
zzAlexzz <zuojun.chen@outlook.com>
2025-10-17 14:02:37 +1000

    chore(app): add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // Custom
+  singleQuote: true,
+  printWidth: 150,
+
+  // Defaults
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import {Text, View} from "react-native";
+import { Text, View } from 'react-native';
 
 export const App = () => {
-  return <View><Text>App</Text></View>;
-}
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
